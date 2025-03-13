e8d689b11b34443bee5d0d668ac829dc4d1e6048
alazad214 <alazad214@gmail.com>
2025-03-13 14:09:24 +0600

    home screen design

--- lib/features/home/views/home_screen.dart
+++ lib/features/home/views/home_screen.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite_tudo_app/utils/colors.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int listItem = 15;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Text('SqfLite Tudo App'),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: ListView.builder(
+          itemCount: listItem,
+          shrinkWrap: true,
+
+          physics: NeverScrollableScrollPhysics(),
+          itemBuilder: (context, index) {
+            return Card(
+              color: Colors.accents[index % Colors.accents.length],
+              child: ListTile(
+                title: Text('Tudo list 1'),
+                subtitle: Text('Tudo list subtitle 1'),
+                leading: CircleAvatar(child: Icon(Icons.list_alt)),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.delete, color: AppColors.white),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add, color: AppColors.green),
       ),
     );
   }
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 
 class AppColors {
   AppColors._();
-
   static const Color primaryColor = Color(0xFF0CC0DF);
   static const Color primaryColorDark = Color(0xFF507687);
   static const Color secondaryColor = Color(0xFFED2D6B);
@@ -12,6 +11,8 @@ class AppColors {
   static const Color black = Color(0xFF000000);
   static const Color white = Color(0xFFFFFFFF);
   static const Color grey = Color(0xFF999999);
+  static const Color red = Color(0xFFFF0000);
+  static const Color green = Color(0xFF008000);
   static const Color accentYellow = Color(0xFFFDB338);
   static const Color darkBlue = Color(0xFF0D1E40);
 }
